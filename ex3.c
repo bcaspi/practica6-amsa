ae6b1a2289ca7197dde9991a91897005235faa32
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2020-12-01 09:06:30 +0100

    fix ex3

--- ex3.c
+++ ex3.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
-double write(const char *filename);
-double read(const char *filename);
+double write_t(const char *filename);
+double read_t(const char *filename);
 
 int main() {
-    
-    double time_spent_write_ram = write("example2.txt");
-    double time_spent_write_sf = write("/tmp/ramdisk/example.txt");
-    double time_spent_read_ram = read("example2.txt");
-    double time_spent_read_sf = read("/tmp/ramdisk/example.txt");
+   
+    double time_spent_write_ram = write_t("example2.txt");
+    printf("Done Writing...\n");
+    double time_spent_write_sf = write_t("/tmp/ramdisk/example.txt");
+    printf("Done Writing...\n");
+    double time_spent_read_ram = read_t("example2.txt");
+    double time_spent_read_sf = read_t("/tmp/ramdisk/example.txt");
 
     printf("%-35s\t %-30lf\n", "Temps escriptura fitxer RAM:", time_spent_write_ram);
     printf("%-35s\t %-30lf\n\n", "Temps escriptura fitxer del sf:", time_spent_write_sf);
@@ -20,7 +23,7 @@ int main() {
     return 0;
 }
 
-double write(const char *filename)        
+double write_t(const char *filename)        
 {
     clock_t begin = clock();
     int i;
@@ -30,29 +33,30 @@ double write(const char *filename)
         printf("Error while opening the file!\n");
         exit(1);
     }
-    for (i = 1; i<100000000; i++){ fprintf(fptr,"%d", i); }
-    fprintf(fptr,"\n");
+    int fd = fileno(fptr);
+    for (i = 1; i<10000000; i++){ write(fd, "a", 1); }
+    write(fd,"\n", 1);
     fclose(fptr);
 
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    return time_spent;             
+    return time_spent;            
 }
 
-double read(const char *filename)        
+double read_t(const char *filename)        
 {
     clock_t begin = clock();
-    
+   
     char ch;
     FILE *fptr;
     if ((fptr = fopen(filename, "r")) == NULL) {
         printf("Error while opening the file!");
         exit(1);
     }
-    while((ch = fgetc(fptr)) != EOF) 
+    while((ch = fgetc(fptr)) != EOF)
     fclose(fptr);
 
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    return time_spent;             
+    return time_spent;            
 }
